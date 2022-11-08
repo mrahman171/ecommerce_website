ca9389d04e5a0243c9469ee5b1a72e1d88f1afa5
A.rahman <mrahman171306@bscse.uiu.ac.bd>
2022-11-08 20:27:21 +0600

    Virtual Component done

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import "./App.css";
 import Header from "./Components/Header/Header.jsx"
 import Hero from "./Components/Hero/Hero"
 import Slider from "./Components/Slider/Slider"
+import Virtual from "./Components/Virtual/Virtual"
 function App() {
   return (
     <div className="App">
        <Header/>
        <Hero/>
        <Slider/> 
-
+       <Virtual/>
     </div>
   );
 }
